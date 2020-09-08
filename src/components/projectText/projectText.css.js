1bf304d460cef8e51fd3131ffa58a5537a990737
Truett Hilliard <truetthilliard@Truetts-MacBook-Pro.local>
2020-09-08 10:00:48 -0500

    make project text responsive

--- src/components/projectText/projectText.css.js
+++ src/components/projectText/projectText.css.js
@@ -4,10 +4,10 @@ import MEDIA from 'helpers/mediaTemplates';
 export const Container = styled.div`
   //   display: inline-grid;
   flex: 1;
+  width 50%
   grid-gap: 4rem;
   padding: 4rem 4rem;
   margin: 2rem 2rem;
-  width: 445px;
   line-height: 1.8rem;
 
   ${MEDIA.TABLET`
